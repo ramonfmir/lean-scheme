1d6623c380dfb35449a94457fe180c5461038541
Ramon Fernandez I Mir <rfernandezmir@gmail.com>
2019-01-28 22:42:41 +0000

    Stalk on basis fixed.

--- src/sheaves/stalk_on_basis.lean
+++ src/sheaves/stalk_on_basis.lean
@@ -1,9 +1,11 @@
 import sheaves.presheaf_of_types_on_basis
 
+open topological_space
+
 universe u 
 
 variables {α : Type u} [T : topological_space α] 
-variables {B : set (set α )} {HB : topological_space.is_topological_basis B}
+variables {B : set (set α )} {HB : is_topological_basis B}
 variables (F : presheaf_of_types_on_basis α HB) (x : α)
 
 structure stalk_on_basis.elem :=
@@ -20,8 +22,6 @@ def stalk_on_basis.relation : stalk_on_basis.elem F x → stalk_on_basis.elem F 
     ∃ W (BW : W ∈ B) (HxW : x ∈ W) (HWU : W ⊆ Us.U) (HWV : W ⊆ Vt.U),
     F.res Us.BU BW HWU Us.s = F.res Vt.BU BW HWV Vt.s
 
--- TODO: CHANGE THIS
-
 lemma stalk_on_basis.relation.reflexive : reflexive (stalk_on_basis.relation F x) :=
 λ ⟨U, OU, HxU, s⟩, ⟨U, OU, HxU, set.subset.refl _, set.subset.refl _, rfl⟩
 
@@ -29,24 +29,25 @@ lemma stalk_on_basis.relation.symmetric : symmetric (stalk_on_basis.relation F x
 λ Us Vt ⟨W, OW, HxW, HWU, HWV, Hres⟩, ⟨W, OW, HxW, HWV, HWU, Hres.symm⟩
 
 lemma stalk_on_basis.relation.transitive : transitive (stalk_on_basis.relation F x) :=
-λ ⟨U, OU, HxU, sU⟩ ⟨V, OV, HxV, sV⟩ ⟨W, OW, HxW, sW⟩,
-λ ⟨R, OR, HxR, HRU, HRV, HresR⟩ ⟨S, OS, HxS, HSV, HSW, HresS⟩,
-⟨R ∩ S, is_open_inter OR OS, ⟨HxR, HxS⟩,
-λ y ⟨HyR, _⟩, HRU HyR, λ y ⟨_, HyS⟩, HSW HyS,
-have ORS : _ := is_open_inter OR OS,
-have HURRS : _ := F.Hcomp OU OR ORS (set.inter_subset_left _ _) HRU,
-have HVRRS : _ := F.Hcomp OV OR ORS (set.inter_subset_left _ _) HRV,
-have HVSRS : _ := F.Hcomp OV OS ORS (set.inter_subset_right _ _) HSV,
-have HWSRS : _ := F.Hcomp OW OS ORS (set.inter_subset_right _ _) HSW,
-calc  F.res OU ORS _ sU 
-    = F.res OR ORS _ (F.res OU OR _ sU) : congr_fun HURRS sU 
-... = F.res OR ORS _ (F.res OV OR _ sV) : congr_arg _ HresR
-... = F.res OV ORS _ sV                 : congr_fun HVRRS.symm sV
-... = F.res OS ORS _ (F.res OV OS _ sV) : congr_fun HVSRS sV
-... = F.res OS ORS _ (F.res OW OS _ sW) : congr_arg _ HresS
-... = F.res OW ORS _ sW                 : congr_fun HWSRS.symm sW⟩
+λ ⟨U, BU, HxU, sU⟩ ⟨V, BV, HxV, sV⟩ ⟨W, BW, HxW, sW⟩,
+λ ⟨R, BR, HxR, HRU, HRV, HresR⟩ ⟨S, BS, HxS, HSV, HSW, HresS⟩,
+let ⟨T, BT, HxT, HTRS⟩ := HB.1 R BR S BS x ⟨HxR, HxS⟩ in
+⟨T, BT, HxT, λ y Hy, HRU (HTRS Hy).1, λ y Hy, HSW (HTRS Hy).2,
+have HTR : T ⊆ R := λ y Hy, (HTRS Hy).1,
+have HTS : T ⊆ S := λ y Hy, (HTRS Hy).2,
+have HURRS : _ := F.Hcomp BU BR BT HTR HRU,
+have HVRRS : _ := F.Hcomp BV BR BT HTR HRV,
+have HVSRS : _ := F.Hcomp BV BS BT HTS HSV,
+have HWSRS : _ := F.Hcomp BW BS BT HTS HSW,
+calc  F.res BU BT _ sU 
+    = F.res BR BT _ (F.res BU BR _ sU) : congr_fun HURRS sU 
+... = F.res BR BT _ (F.res BV BR _ sV) : congr_arg _ HresR
+... = F.res BV BT _ sV                 : congr_fun HVRRS.symm sV
+... = F.res BS BT _ (F.res BV BS _ sV) : congr_fun HVSRS sV
+... = F.res BS BT _ (F.res BW BS _ sW) : congr_arg _ HresS
+... = F.res BW BT _ sW                 : congr_fun HWSRS.symm sW⟩
 
-lemma stalk_on_basis.relation.equivalence : equivalence (stalk.relation F x) :=
+lemma stalk_on_basis.relation.equivalence : equivalence (stalk_on_basis.relation F x) :=
 ⟨stalk_on_basis.relation.reflexive F x, 
 stalk_on_basis.relation.symmetric F x,
 stalk_on_basis.relation.transitive F x⟩
