f6687b72bff166c8b04489a34ed754447902d82e
Ramon Fernandez I Mir <rfernandezmir@gmail.com>
2019-02-07 20:13:37 +0000

    Extension proof working again with new sheaf condition and opens.

--- src/sheaves/sheaf_of_types_on_basis.lean
+++ src/sheaves/sheaf_of_types_on_basis.lean
@@ -55,16 +55,101 @@ definition presheaf_of_types_on_basis_to_presheaf_of_types
   Hid := λ U, funext $ λ x, subtype.eq rfl,
   Hcomp := λ U V W HWV HVU, funext $ λ x, subtype.eq rfl}
 
+local notation F `ₑₓₜ`:1 := 
+presheaf_of_types_on_basis_to_presheaf_of_types F
+
 instance forget_basis : has_coe (presheaf_of_types_on_basis α HB) (presheaf_of_types α) :=
 ⟨presheaf_of_types_on_basis_to_presheaf_of_types⟩
- 
+
+
 -- Presheaf extension preserves sheaf condition.
--- TODO: Factor out all of the classical indefinite decription stuff.
+
+noncomputable def global_section 
+(F : presheaf_of_types_on_basis α HB) (U : opens α) (OC : covering U) 
+(s : Π i, (F ₑₓₜ) (OC.Uis i))
+(Hsec : ∀ (j k : OC.γ),
+  res_to_inter_left (F ₑₓₜ) (OC.Uis j) (OC.Uis k) (s j) =
+  res_to_inter_right (F ₑₓₜ) (OC.Uis j) (OC.Uis k) (s k))
+: {r : Π (x ∈ U), stalk_on_basis F x //
+∀ (x ∈ U), ∃ (V) (BV : V ∈ B) (Hx : x ∈ V) (σ : F BV),
+∀ (y ∈ U ∩ V), r y = λ _, ⟦{U := V, BU := BV, Hx := H.2, s := σ}⟧} :=
+begin 
+refine ⟨_, _⟩,
+{ -- Define s.
+  intros x HxU,
+  rw OC.Hcov.symm at HxU,
+  rcases (classical.indefinite_description _ HxU) with ⟨Uk, HUk⟩,
+  rcases (classical.indefinite_description _ HUk) with ⟨HUkUis, HxUk⟩,
+  rcases (classical.indefinite_description _ HUkUis) with ⟨OUk, ⟨HOUkUis, HUkeq⟩⟩,
+  rcases (classical.indefinite_description _ HOUkUis) with ⟨k, HUiskeq⟩,
+  rw HUkeq.symm at HxUk,
+  rw HUiskeq.symm at HxUk,
+  exact (s k).val x HxUk },
+{ -- Prove the property of s.
+  intros x HxU,
+  erw OC.Hcov.symm at HxU,
+  rcases HxU with ⟨Uk, ⟨⟨OUk, ⟨⟨k, HUiskeq⟩, HUkeq⟩⟩, HxUk⟩⟩,
+  rw HUkeq.symm at HxUk,
+  rw HUiskeq.symm at HxUk,
+  rcases (s k).property x HxUk with ⟨V, ⟨BV, ⟨HxV, ⟨σ, Hσ⟩⟩⟩⟩,
+  -- We find W ∈ B such that x ∈ W and W ⊆ V ∩ Ui k.
+  have HxVUik : x ∈ (V ∩ OC.Uis k) := ⟨HxV, HxUk⟩,
+  have OVUik := is_open_inter V.2 (OC.Uis k).2,
+  have HVUik := mem_nhds_sets OVUik HxVUik,
+  have HW := (mem_nhds_of_is_topological_basis HB).1 HVUik,
+  rcases HW with ⟨W, BW, ⟨HxW, HWVUk⟩⟩,
+  simp at BW,
+  rcases BW with ⟨OW, BW⟩,
+  -- We now find the right σ' ∈ F(W).
+  have HWV := (set.subset.trans HWVUk $ set.inter_subset_left _ _),
+  let σ' := F.res BV BW HWV σ,
+  -- Exists (W, σ') and proceed. 
+  use [⟨W, OW⟩, BW, HxW, σ'],
+  rintros y ⟨HyU, HyW⟩,
+  have HyVUik : y ∈ (V ∩ OC.Uis k) := HWVUk HyW,
+  apply funext,
+  intros HyU; dsimp,
+  -- Now we need to show that ⟦(s k, Ui k)⟧ corresponds to ⟦(σ', W)⟧.
+  have Hsk := Hσ y HyVUik.symm,
+  let HyUi := λ t, ∃ (H : t ∈ subtype.val '' set.range OC.Uis), y ∈ t,
+  rcases (classical.indefinite_description HyUi _) with ⟨S, HS⟩; dsimp,
+  let HyS := λ H : S ∈ subtype.val '' set.range OC.Uis, y ∈ S,
+  rcases (classical.indefinite_description HyS HS) with ⟨HSUiR, HySUiR⟩; dsimp,
+  let HOUksub := λ t : subtype is_open, t ∈ set.range (OC.Uis) ∧ t.val = S,
+  rcases (classical.indefinite_description HOUksub _) with ⟨OUl, ⟨HOUl, HOUleq⟩⟩; dsimp,
+  let HSUi := λ i, OC.Uis i = OUl,
+  cases (classical.indefinite_description HSUi _) with l HSUil; dsimp,
+  -- We finally have (s l).val y _ = ⟦(W, σ')⟧.
+  have HyOUk : y ∈ OUl.val := HOUleq.symm ▸ HySUiR,
+  have HyUil : y ∈ OC.Uis l := HSUil.symm ▸ HyOUk,
+  have HyUik : y ∈ OC.Uis k := HyVUik.2,
+  suffices Hsuff : (s l).val y HyUil = (s k).val y HyUik,
+    erw Hsuff,
+    rw Hsk,
+    apply quotient.sound,
+    use [⟨W, OW⟩, BW, HyW, HWV],
+    existsi (set.subset.refl _),
+    simp,
+    apply F.Hcomp',
+  -- Proving Hsuff.
+  let F' := presheaf_of_types_on_basis_to_presheaf_of_types F,
+  let UkUl := OC.Uis k ∩ OC.Uis l,
+  have Hslres : (s l).val y HyUil = 
+    (F'.res (OC.Uis l) UkUl (set.inter_subset_right _ _) (s l)).val y ⟨HyUik, HyUil⟩ := rfl,
+  have Hskres : (s k).val y HyUik = 
+    (F'.res (OC.Uis k) UkUl (set.inter_subset_left _ _) (s k)).val y ⟨HyUik, HyUil⟩ := rfl,
+  have Hs := Hsec k l,
+  unfold res_to_inter_left at Hs,
+  unfold res_to_inter_right at Hs,
+  erw [Hslres, Hskres, Hs],
+  apply congr_arg, 
+  simp }
+end
 
 theorem extension_is_sheaf
   (F : presheaf_of_types_on_basis α HB) 
   (HF : is_sheaf_of_types_on_basis F)
-  : is_sheaf_of_types (presheaf_of_types_on_basis_to_presheaf_of_types F) := 
+  : is_sheaf_of_types (F ₑₓₜ) := 
 begin
   split,
   -- Locality.
@@ -86,102 +171,44 @@ begin
   },
   -- Gluing.
   { intros U OC s Hsec,
-    existsi _,
-
-    existsi _, swap,
-    { refine ⟨_, _⟩,
-      { intros x HxU,
-        rw OC.Hcov'.symm at HxU,
-        cases (classical.indefinite_description _ HxU) with Uk HUk,
-        cases (classical.indefinite_description _ HUk) with HUk HxUk,
-        cases (classical.indefinite_description _ HUk) with Uk1 HUk,
-        rcases HUk with ⟨HUk, Heq⟩,
-        cases (classical.indefinite_description _ HUk) with k HUk,
-        rw ←HUk at HxUk,
-        exact (s.val k).val x HxUk,
-      },
-      { intros x HxU,
-        erw OCU.symm at HxU,
-        rcases HxU with ⟨Uk, ⟨⟨k, HUk⟩, HxUk⟩⟩,
-        erw ←HUk at HxUk,
-        rcases (s.val k).property x HxUk with ⟨V, ⟨BV, ⟨HxV, ⟨σ, Hσ⟩⟩⟩⟩,
-        -- We find W ∈ B such that x ∈ W and W ⊆ V ∩ Ui k.
-        have HxVUik : x ∈ (V ∩ OC.Ui k) := ⟨HxV, HxUk⟩,
-        have OVUik := T.is_open_inter V (OC.Ui k) (open_basis_elem BV) (OC.OUi k),
-        have HVUik := mem_nhds_sets OVUik HxVUik,
-        have HW := (mem_nhds_of_is_topological_basis HB).1 HVUik,
-        rcases HW with ⟨W, BW, ⟨HxW, HWVUik⟩⟩,
-        -- We now find the right σ' ∈ F(W).
-        have HWV := (set.subset.trans HWVUik $ set.inter_subset_left _ _),
-        let σ' := F.res BV BW HWV σ,
-        -- Exists (W, σ') and proceed. 
-        use [W, BW, HxW, σ'],
-        rintros y ⟨HyU, HyW⟩,
-        have HyVUik : y ∈ (V ∩ OC.Ui k) := HWVUik HyW,
-        apply funext,
-        intros HyU; dsimp,
-        -- Now we need to show that ⟦(s k, Ui k)⟧ corresponds to ⟦(σ', W)⟧.
-        have Hsk := Hσ y HyVUik.symm,
-        let HyUi := λ t, ∃ (H : t ∈ set.range OC.Ui), y ∈ t,
-        cases (classical.indefinite_description HyUi _) with S HS; dsimp,
-        let HyS := λ H : S ∈ set.range OC.Ui, y ∈ S,
-        cases (classical.indefinite_description HyS HS) with HSUiR HySUiR; dsimp,
-        let HSUi := λ i, OC.Ui i = S,
-        cases (classical.indefinite_description HSUi HSUiR) with l HSUil; dsimp,
-        -- We finally have (s l).val y _ = ⟦(W, σ')⟧.
-        have HyUil : y ∈ OC.Ui l := HSUil.symm ▸ HySUiR,
-        have HyUik : y ∈ OC.Ui k := HyVUik.2,
-        suffices Hsuff : (s.val l).val y HyUil = (s.val k).val y HyUik,
-          rw Hsuff,
-          rw Hsk,
-          apply quotient.sound,
-          use [W, BW, HyW, HWV],
-          existsi _,
-          simp,
-        -- Proving Hsuff.
-        have OUikUil : T.is_open (OC.Ui k ∩ OC.Ui l) :=
-            T.is_open_inter _ _ (OC.OUi k) (OC.OUi l),
-        have HUikUilUil := set.inter_subset_right (OC.Ui k) (OC.Ui l),
-        have HUikUilUik := set.inter_subset_left (OC.Ui k) (OC.Ui l),
-        let F' := presheaf_of_types_on_basis_to_presheaf_of_types F,
-        have Hslres : (s.val l).val y HyUil = 
-          (F'.res (OC.OUi l) OUikUil HUikUilUil (s.val l)).val y ⟨HyUik, HyUil⟩ := rfl,
-        have Hskres : (s.val k).val y HyUik = 
-          (F'.res (OC.OUi k) OUikUil HUikUilUik (s.val k)).val y ⟨HyUik, HyUil⟩ := rfl,
-        have Hs := s.property k l,
-        unfold presheaf_of_types.res_to_inter_left at Hs,
-        unfold presheaf_of_types.res_to_inter_right at Hs,
-        rw [Hslres, Hskres, Hs] 
-      },
-    },
+    existsi (global_section F U OC s Hsec),
     { -- S|i = s_i for all i.
-      intros i, simp,
-      apply subtype.eq; dsimp,
+      intros i,
+      apply subtype.eq,
       apply funext,
       intros x,
       apply funext,
       intros HxUi,
-      have HxU : x ∈ U := OCU ▸ (set.subset_Union OC.Ui i) HxUi,
-      let HyUi := λ t, ∃ (H : t ∈ set.range OC.Ui), x ∈ t,
+      have HxU : x ∈ U := OC.Hcov ▸ (opens_supr_subset OC.Uis i) HxUi,
+      let HyUi := λ t, ∃ (H : t ∈ set.range OC.Uis), x ∈ t,
       dunfold presheaf_of_types_on_basis_to_presheaf_of_types,
       dsimp,
-      cases (classical.indefinite_description HyUi _) with S HS,dsimp,
-      let HyS := λ H : S ∈ set.range OC.Ui, x ∈ S,
-      cases (classical.indefinite_description HyS HS) with HSUiR HySUiR; dsimp,
-      let HSUi := λ i, OC.Ui i = S,
-      cases (classical.indefinite_description HSUi HSUiR) with l HSUil; dsimp,
-      have HxUlUi : x ∈ OC.Ui i ∩ OC.Ui l := ⟨HxUi, HSUil.symm ▸ HySUiR⟩,
-      have X := s.property i l,
-      dunfold presheaf_of_types_on_basis_to_presheaf_of_types at X,
-      dunfold presheaf_of_types.res_to_inter_left at X,
-      dunfold presheaf_of_types.res_to_inter_right at X,
-      dsimp at X,
-      rw subtype.ext at X,
-      dsimp at X,
-      replace X := congr_fun X x,
-      dsimp at X,
-      replace X := congr_fun X,
-      exact (X HxUlUi).symm, }
+      dunfold global_section,
+      dsimp,
+      let HyUi := λ t, ∃ (H : t ∈ subtype.val '' set.range OC.Uis), x ∈ t,
+      rcases (classical.indefinite_description HyUi _) with ⟨S, HS⟩; dsimp,
+      let HyS := λ H : S ∈ subtype.val '' set.range OC.Uis, x ∈ S,
+      rcases (classical.indefinite_description HyS HS) with ⟨HSUiR, HySUiR⟩; dsimp,
+      let HOUksub := λ t : subtype is_open, t ∈ set.range (OC.Uis) ∧ t.val = S,
+      rcases (classical.indefinite_description HOUksub _) with ⟨OUl, ⟨HOUl, HOUleq⟩⟩; dsimp,
+      let HSUi := λ i, OC.Uis i = OUl,
+      cases (classical.indefinite_description HSUi _) with l HSUil; dsimp,
+
+      dunfold presheaf_of_types_on_basis_to_presheaf_of_types at Hsec,
+      dunfold res_to_inter_left at Hsec,
+      dunfold res_to_inter_right at Hsec,
+      dsimp at Hsec,
+      replace Hsec := Hsec i l,
+      rw subtype.ext at Hsec,
+      dsimp at Hsec,
+      replace Hsec := congr_fun Hsec x,
+      dsimp at Hsec,
+      replace Hsec := congr_fun Hsec,
+
+      have HxOUk : x ∈ OUl.val := HOUleq.symm ▸ HySUiR,
+      have HxUl : x ∈ OC.Uis l := HSUil.symm ▸ HxOUk,
+      
+      exact (Hsec ⟨HxUi, HxUl⟩).symm }
   },
 end 
 
