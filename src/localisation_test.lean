3555a9db9ac2b1ec9324cf436248456d98d21735
Ramon Fernandez I Mir <rfernandezmir@gmail.com>
2019-02-13 14:14:03 +0000

    Finished proving test.

--- src/localisation_test.lean
+++ src/localisation_test.lean
@@ -1,14 +1,13 @@
-
-import data.set.basic
 import ring_theory.localization
 
-import preliminaries.localisation
+-- Kenny's refactoring of Neil's code.
+import preliminaries.localisation 
 
 universes u v w
 
 -- Define localisation on a set of generators.
 
-section localisation_on_generators 
+section localization_on_generators 
 
 parameters {A : Type u} {B : Type v} {C : Type w} 
 parameters [comm_ring A] [comm_ring B] [comm_ring C]
@@ -18,43 +17,18 @@ def S : set A := monoid.closure G
 instance S_submonoid : is_submonoid S := monoid.closure.is_submonoid G
 
 def is_localization' := localization_alt.is_localization S f
+def is_localization_data' := localization_alt.is_localization_data S f
 
-end localisation_on_generators
+end localization_on_generators
 
-/-
- Trying to fix issues in:
- https://github.com/kbuzzard/lean-stacks-project/blob/595aa1d6c5ce5a6fa0259c5a0a2226a91b07d96e/src/canonical_isomorphism_nonsense.lean#L268
+-- We show that R[1/a][1/b] = R[1/ab], i.e. is_localization' R[1/a][1/b] (a*b).
 
- We want to prove the sheaf condition on the presheaf defined on the standard 
- opens D(fi) as in https://stacks.math.columbia.edu/tag/01HR.
-
- In the proof, they use Lemma 10.23.1/2 https://stacks.math.columbia.edu/tag/00EI
- which is when the "canonical isomorphism nonsense" was used.
-
- We should start testing this new API with the lemmas on localisation_UMP.
--/
-
--- Important result.
-
-section
+section localization_ab
 
 parameters {R : Type u} [comm_ring R]
 parameters (a b : R)
 
--- Generic result.
-
--- Definitely true...
-
-lemma closure_mem {S : set R} :
-∀ {x y : R}, x ∈ monoid.closure S → y ∈ monoid.closure S → (x * y) ∈ monoid.closure S :=
-sorry
-
-lemma closure_mem2 :
-∀ {x y : R}, 
-x ∈ monoid.closure ({a} : set R) → 
-y ∈ monoid.closure ({b} : set R) → 
-(x * y) ∈ monoid.closure ({a * b} : set R) :=
-sorry
+-- I need this because localization on generators is defined using monoid.closure.
 
 lemma powers_closure_eq : ∀ x : R, powers x = monoid.closure {x} :=
 begin
@@ -92,7 +66,7 @@ begin
       exact ⟨n + m, by simp⟩, } }
 end
 
--- R[1/a]
+-- Definition of R[1/a].
 
 def Ra := localization.away a
 
@@ -101,21 +75,20 @@ instance powers_a_submonoid : is_submonoid (powers a) := powers.is_submonoid a
 
 lemma one_powers_a : (1:R) ∈ powers a := powers_a_submonoid.one_mem
 
--- A[1/a][1/b]
+-- Definition of R[1/a][1/b].
 
-def b' : Ra := ⟦⟨b, 1, one_powers_a⟩⟧ 
+def b1 : Ra := ⟦⟨b, 1, one_powers_a⟩⟧ 
 
-def Rab := localization.loc Ra (powers b')
-instance Afg_comm_ring : comm_ring Rab := localization.away.comm_ring b'
-instance powers_b'_submonoid : is_submonoid (powers b') := powers.is_submonoid b'
+def Rab := localization.loc Ra (powers b1)
+instance Afg_comm_ring : comm_ring Rab := localization.away.comm_ring b1
+instance powers_b1_submonoid : is_submonoid (powers b1) := powers.is_submonoid b1
 
-lemma one_powers_b' : (1:Ra) ∈ powers b' := powers_b'_submonoid.one_mem
+lemma one_powers_b1 : (1:Ra) ∈ powers b1 := powers_b1_submonoid.one_mem
 
-lemma one_powers_b'_mk : (1:Ra) = ⟦⟨1, 1, one_powers_a⟩⟧ :=
-begin apply quotient.sound; use [1, one_powers_a]; simp end
+-- Basically saying that b1 is b/1.
 
-lemma elem_powers_b'_bn
-: ∀ x : Ra, x ∈ powers b' ↔ ∃ n : ℕ, x = ⟦⟨b ^ n, 1, one_powers_a⟩⟧ :=
+lemma elem_powers_b1_bn
+: ∀ {x : Ra}, x ∈ powers b1 ↔ ∃ n : ℕ, x = ⟦⟨b ^ n, 1, one_powers_a⟩⟧ :=
 begin
   intros x,
   split,
@@ -128,7 +101,7 @@ begin
     { intros x Hx,
       rw pow_succ at *,
       rw ←Hx,
-      rw Hn (b' ^ n); try { refl },
+      rw @Hn (b1 ^ n); try { refl },
       apply quotient.sound; use [1, one_powers_a]; simp, } },
   { rintros ⟨n, Hx⟩,
     existsi n,
@@ -138,49 +111,51 @@ begin
     { intros x Hx,
       rw pow_succ at *,
       rw Hx,
-      rw Hn (⟦(b ^ n, ⟨1, _⟩)⟧); try { refl },
+      rw @Hn (⟦(b ^ n, ⟨1, _⟩)⟧); try { refl },
       apply quotient.sound; use [1, one_powers_a]; simp, } }
 end
 
-lemma elem_powers_b'
-: ∀ x : Ra, x ∈ powers b' ↔ ∃ y ∈ powers b, x = ⟦⟨y, 1, one_powers_a⟩⟧ :=
+lemma elem_powers_b1
+: ∀ {x : Ra}, x ∈ powers b1 ↔ ∃ y ∈ powers b, x = ⟦⟨y, 1, one_powers_a⟩⟧ :=
 begin
   intros x,
   split,
   { intros Hx,
-    have Hbn := (elem_powers_b'_bn x).1 Hx,
+    have Hbn := elem_powers_b1_bn.1 Hx,
     rcases Hbn with ⟨n, Hx⟩,
     use [b ^ n, ⟨n, rfl⟩],
     exact Hx, },
   { rintros ⟨y, ⟨n, Hy⟩, Hx⟩,
     rw ←Hy at Hx,
-    apply (elem_powers_b'_bn x).2 ⟨n, Hx⟩, }
+    apply elem_powers_b1_bn.2 ⟨n, Hx⟩, }
 end
 
--- h : A --> A[1/ab]
+-- Localization map A --> A[1/a][1/b].
 
-@[reducible] def h : R → Rab := λ x, ⟦⟨⟦⟨x, 1, one_powers_a⟩⟧, 1, one_powers_b'⟩⟧
+@[reducible] def h : R → Rab := λ x, ⟦⟨⟦⟨x, 1, one_powers_a⟩⟧, 1, one_powers_b1⟩⟧
 instance is_ring_hom_h : is_ring_hom h :=
 { map_one := rfl,
   map_add := λ x y,
   begin
-    apply quotient.sound; use [1, one_powers_b']; simp,
+    apply quotient.sound; use [1, one_powers_b1]; simp,
     apply quotient.sound; use [1, one_powers_a]; simp,
   end,
   map_mul := λ x y,
   begin
-    apply quotient.sound; use [1, one_powers_b']; simp,
+    apply quotient.sound; use [1, one_powers_b1]; simp,
     apply quotient.sound; use [1, one_powers_a]; simp,
   end}
 
--- THE MAIN RESULT
+-- Proving the lemmas: inverts, denom and ker = ann.
+
+section localization_ab_proof
 
 -- Inverses.
 
-@[reducible] def one_Rab : Rab := 1
+def one_Rab : Rab := 1
 
-def binv_Rab : Rab := ⟦⟨⟦⟨1, 1, one_powers_a⟩⟧, b', ⟨1, by simp⟩⟩⟧
-def ainv_Rab : Rab := ⟦⟨⟦⟨1, a, ⟨1, by simp⟩⟩⟧, 1, one_powers_b'⟩⟧
+def binv_Rab : Rab := ⟦⟨⟦⟨1, 1, one_powers_a⟩⟧, b1, ⟨1, by simp⟩⟩⟧
+def ainv_Rab : Rab := ⟦⟨⟦⟨1, a, ⟨1, by simp⟩⟩⟧, 1, one_powers_b1⟩⟧
 
 lemma loc_inverts : localization_alt.inverts (S {a * b}) h :=
 begin
@@ -189,13 +164,15 @@ begin
   case monoid.in_closure.basic : y Hy 
   { rcases Hy with ⟨Hya, Hyb⟩,
     { existsi (ainv_Rab * binv_Rab),
-      apply quotient.sound; use [1, one_powers_b']; simp,
+      apply quotient.sound; use [1, one_powers_b1]; simp,
       apply quotient.sound; use [1, one_powers_a]; simp,
       rw Hy; simp, },
     { cases Hy, } },
   case monoid.in_closure.one
   { existsi one_Rab,
-    simp, rw is_ring_hom_h.map_one, },
+    simp,
+    rw is_ring_hom_h.map_one, 
+    simpa, },
   case monoid.in_closure.mul : x y HxS HyS Hx Hy
   { rcases Hx with ⟨xinv, Hxinv⟩,
     rcases Hy with ⟨yinv, Hyinv⟩,
@@ -208,6 +185,45 @@ end
 
 -- Denoms.
 
+def homogenizer (n m : ℕ) : R := if n ≤ m then a^(m - n) else b^(n - m)
+
+lemma homogenizer_a {n m : ℕ} : n ≤ m → homogenizer n m = a^(m - n) :=
+begin
+  intros Hnm,
+  dunfold homogenizer,
+  rw if_eq_of_eq_true,
+  apply eq_true_intro Hnm,
+end
+
+lemma homogenizer_b {n m : ℕ} : ¬(n ≤ m) → homogenizer n m = b^(n - m) := 
+begin
+  intros Hnm,
+  dunfold homogenizer,
+  rw if_eq_of_eq_false,
+  apply eq_false_intro Hnm,
+end
+
+lemma homogenizer_mul : ∀ n m, a^n * b^m * homogenizer n m = a^(max n m) * b^(max n m) :=
+begin
+  intros n m,
+  cases (classical.em (n ≤ m)) with Htt Hff,
+  { rw homogenizer_a Htt,
+    rw max_eq_right Htt,
+    rw mul_assoc,
+    rw mul_comm,
+    rw mul_assoc,
+    rw ←pow_add,
+    rw nat.sub_add_cancel Htt,
+    rw mul_comm, },
+  { rw homogenizer_b Hff,
+    replace Hff := le_of_not_le Hff,
+    rw max_eq_left Hff,
+    rw mul_assoc,
+    rw ←pow_add,
+    rw add_comm,
+    rw nat.sub_add_cancel Hff, }
+end
+
 lemma loc_has_denom : localization_alt.has_denom (monoid.closure {a * b}) h :=
 begin
   intros x,
@@ -217,83 +233,82 @@ begin
   rintros ⟨xR, xdenRa⟩,
   rcases xdenRa with ⟨y, Hy⟩,
   rcases xdenRab with ⟨w, Hw⟩,
-  have Hb := (elem_powers_b' w).1 Hw,
+  have Hb := elem_powers_b1.1 Hw,
   rcases Hb with ⟨z, Hz, Hweq⟩,
   have Hy' := Hy,
   rcases Hy with ⟨n, Hy⟩,
   rcases Hz with ⟨m, Hz⟩,
-  cases (classical.em (n ≤ m)),
-  { have HyzkSab : (a * b)^m ∈ powers (a * b) := ⟨m, rfl⟩,
-    have Habk : (a * b)^m = a^m * b^m := by rw mul_pow,
-    rw powers_closure_eq at HyzkSab,
-    use (⟨(a * b)^m, HyzkSab⟩, xR * a ^ (m - n) ); apply quotient.sound,
-    use [w, Hw]; simp,
-    rw Hweq,
-    apply quotient.sound; simp,
-    use [y, Hy']; simp,
-    rw [←Hy, ←Hz],
-    have H : a ^ n * a ^ (m - n) = a^m,
-      rw [←pow_add, nat.add_sub_of_le],
-      exact h,
-    rw mul_comm xR,
-    rw ←mul_assoc (b^m),
-    rw mul_comm (b^m),
-    rw ←mul_assoc (a^n),
-    rw ←mul_assoc (a^n),
-    rw H,
-    rw Habk,
-    rw add_left_neg,
-    simp, },
-  { sorry }
+  have HyzkSab : (a * b)^(max n m) ∈ powers (a * b) := ⟨(max n m), rfl⟩,
+  rw powers_closure_eq at HyzkSab,
+  use (⟨(a * b)^(max n m), HyzkSab⟩, xR * homogenizer n m),
+  apply quotient.sound; use [w, Hw]; simp,
+  rw Hweq,
+  apply quotient.sound; use [y, Hy']; simp,
+  rw [←Hy, ←Hz],
+  rw mul_comm xR,
+  rw ←mul_assoc (b^m),
+  rw ←mul_assoc (a^n),
+  rw ←mul_assoc (a^n),
+  rw homogenizer_mul n m,
+  rw mul_pow a b,
+  simp, 
 end
 
 -- Kernel is monoid annihilator.
--- TODO: Clena this up this is quite bad.
 
-lemma loc_ker_ann : localization_alt.ker h ≤ localization_alt.submonoid_ann (monoid.closure {a, b}) :=
+lemma loc_ker_ann_sub : localization_alt.ker h ≤ localization_alt.submonoid_ann (S {a * b}) :=
 λ x Hx,
 begin
   unfold localization_alt.submonoid_ann,
   unfold localization_alt.ker at *,
   unfold localization_alt.ideal.mk at *,
   have Hx : x ∈ {x : R | h x = 0} := Hx,
-  have H : h x = 0 := by apply_assumption,
-  dsimp,
+  have Hx0 : h x = 0 := by apply_assumption,
   dunfold localization_alt.ann_aux,
   unfold set.range,
   simp,
-  show ∃ (a_1 x_1 : R), x_1 ∈ monoid.closure (insert b {a}) ∧ a_1 * x_1 = 0 ∧ a_1 = x,
+  show ∃ (c z : R), z ∈ S {a * b} ∧ c * z = 0 ∧ c = x,
     existsi x,
-    dunfold h at H,
-    dsimp at H,
-    have H4 := quotient.exact H,
-    rcases H4 with ⟨q, ⟨w, e⟩⟩,
-    simp at e,
-    have H5 := (elem_powers_b' q).1 w,
-    rcases H5 with ⟨r, t, y⟩,
-    rw y at e,
-    have H6 := quotient.exact e,
-    rcases H6 with ⟨u,i,o⟩,
-    simp at o,
-    existsi (r * u),
+    dunfold h at Hx0,
+    have Hx01 := quotient.exact Hx0,
+    rcases Hx01 with ⟨t, Ht, Hx01⟩,
+    simp at Hx01,
+    have Hty1 := elem_powers_b1.1 Ht,
+    rcases Hty1 with ⟨y, Hy, Hty1⟩,
+    rw Hty1 at Hx01,
+    have Hxy0 := quotient.exact Hx01,
+    rcases Hxy0 with ⟨z, Hz, Hxyz0⟩,
+    simp at Hxyz0,
+    rcases Hy with ⟨m, Hy⟩,
+    rcases Hz with ⟨n, Hz⟩,
+    existsi (z * y * homogenizer n m),
     split,
-      rw powers_closure_eq at t,
-      rw powers_closure_eq at i,
-      have G := closure_mem2 i t,
-      rw mul_comm,
-      exact G,
-
-      split,
+    { rw [←Hy, ←Hz],
+      rw homogenizer_mul n m,
+      rw ←mul_pow,
+      unfold S,
+      rw ←powers_closure_eq,
+      exact ⟨max n m, rfl⟩, },
+    { split; try { refl },
       rw ←mul_assoc,
-      exact o,
-
-      refl,
+      rw mul_comm z,
+      rw ←mul_assoc,
+      rw Hxyz0,
+      simp, }
 end
 
-lemma loc_ker_ann' : localization_alt.ker h = localization_alt.submonoid_ann (monoid.closure {a, b}) :=
-sorry
-
-lemma loc_Afg : is_localization' {a, b} h :=
-⟨loc_inverts, loc_has_denom, loc_ker_ann'⟩
-
+lemma loc_ker_ann : localization_alt.ker h = localization_alt.submonoid_ann (S {a * b}) :=
+begin
+  apply le_antisymm,
+  { exact loc_ker_ann_sub, },
+  { exact localization_alt.inverts_ker (S {a * b}) h loc_inverts, }
 end
+
+-- R[1/a][1/b] = R[1/ab].
+
+lemma loc_Rab : is_localization' {a * b} h :=
+⟨loc_inverts, loc_has_denom, loc_ker_ann⟩
+
+end localization_ab_proof
+
+end localization_ab
