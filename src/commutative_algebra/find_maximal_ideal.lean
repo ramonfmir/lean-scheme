989db09c8c726e15fbbf46032725f8999b996734
Ramon Fernandez I Mir <rfernandezmir@gmail.com>
2019-02-26 12:23:47 +0000

    Found maximal ideal.

--- src/commutative_algebra/find_maximal_ideal.lean
+++ src/commutative_algebra/find_maximal_ideal.lean
@@ -1,3 +1,9 @@
+/-
+  Kenny needed this to prove properties about Spec.
+
+  Is this done better anywhere in the mathlib??
+-/
+
 import ring_theory.ideals
 
 noncomputable theory
@@ -7,9 +13,9 @@ universe u
 
 open lattice
 
-section maximal_ideal
+section find_maximal_ideal
 
-parameters {α : Type u} [comm_ring α] (P : ideal α)
+parameters {α : Type u} [comm_ring α] (P : ideal α) [HPnT : P ≠ ⊤]
 
 instance ideal.has_subset : has_subset (ideal α) :=
 { subset := λ I J, I.1 ⊆ J.1, }
@@ -20,26 +26,24 @@ instance ideal.partial_order : partial_order (ideal α) :=
   le_trans := λ I J K, by apply set.subset.trans,
   le_antisymm := λ I J HIJ HJI, ideal.ext $ λ x, ⟨λ Hx, HIJ Hx, λ Hx, HJI Hx⟩, }
 
-instance find_maximal_ideal.partial_order : partial_order {S : ideal α // P ⊆ S} := 
+instance find_maximal_ideal.partial_order : partial_order {S : ideal α // P ⊆ S ∧ S ≠ ⊤} := 
 begin
   apply subtype.partial_order _,
   apply_instance,
 end
 
-instance find_maximal_ideal.inhabited : inhabited {S : ideal α // P ⊆ S} :=
-⟨⟨P, set.subset.refl P⟩⟩
+instance find_maximal_ideal.inhabited : inhabited {S : ideal α // P ⊆ S ∧ S ≠ ⊤} :=
+⟨⟨P, set.subset.refl P, HPnT⟩⟩
 
-#print set.coe_nonempty_iff_ne_empty
-#print classical.inhabited_of_nonempty
-#check ideal.add_mem
+include HPnT
 
 private theorem find_maximal_ideal.aux :
-  ∃ (M : {S : ideal α // P ⊆ S}), ∀ x, M ≤ x → x = M :=
+  ∃ (M : {S : ideal α // P ⊆ S ∧ S ≠ ⊤}), ∀ x, M ≤ x → x = M :=
 begin
   apply zorn.zorn_partial_order,
   intros c Hc,
   cases (classical.em (c = ∅)) with Hce Hce,
-  { use ⟨P, set.subset.refl P⟩,
+  { use ⟨P, set.subset.refl P, HPnT⟩,
     rw Hce,
     intros a Ha,
     cases Ha, },
@@ -67,113 +71,74 @@ begin
       rw [←HJval, ←h, HIval] at HyJ,
       exact (ideal.add_mem I HxI HyJ), },
     have Hzn := Hc HI HIc HJ HJc h,
+    rw ←HJval at HyJ,
+    rw ←HIval at HxI,
     cases Hzn,
     { use J,
       simp,
       rw ←HJval, 
       use HJ.2,
+      { simp,
+        use HJc, },
+      replace HxI := Hzn HxI,
+      apply ideal.add_mem _ HxI HyJ, },
+    { use I,
       simp,
-      use HJc, },
-    {  }
-  },
+      rw ←HIval,
+      use HI.2,
+      { simp,
+        use HIc, },
+      replace HyJ := Hzn HyJ,
+      apply ideal.add_mem _ HxI HyJ, } },
   { -- smul 
-  },
-  { intros x Hx,
-    have HS : ∀ {S}, S ∈ c → x ∈ (subtype.val S),
-      rintros ⟨S, PS⟩ HSc,
-      exact PS Hx,
-    intros y Hy,
-    simp at Hy,
-    rcases Hy with ⟨z, Hz⟩,
-    rw ←Hz,
-    intros w Hw,
-    simp at Hw,
-    rcases Hw with ⟨Hw1, Hw2⟩,
-
-    },
-  {}
+    intros k x Hx, 
+    rcases Hx with ⟨I, ⟨HI, ⟨HIc, HIval⟩⟩, HxI⟩,
+    use I,
+    rw ←HIval,
+    simp,
+    use [HI.2, HIc],
+    rw ←HIval at HxI,
+    apply ideal.mul_mem_left _ HxI, },
+  -- We have that ⋃ In is an ideal. Now we prove that it is an upper bound.
+  { split,
+    { intros x Hx,
+      simp,
+      use [Q.1, Q.2],
+      { simp,
+        use HQ, },
+      exact (Q.2.1 Hx), },
+    { intros HC,
+      have Hone := (ideal.eq_top_iff_one _).1 HC,
+      rcases Hone with ⟨I, ⟨HI, ⟨HIc, HIval⟩⟩, Hone⟩,
+      apply HI.2.2,
+      rw HIval,
+      exact (ideal.eq_top_iff_one _).2 Hone, }, },
+  { intros a Ha x Hx,
+    use a.1,
+    simp,
+    exact ⟨⟨a.2, Ha⟩, Hx⟩, }
 end
 
--- λ c x hx hc, ⟨⟨{y | ∃ S : {S : set α // P ⊆ S ∧ is_proper_ideal S}, S ∈ c ∧ y ∈ S.val},
---   λ z hz, ⟨x, hx, x.2.1 hz⟩,
---   { zero_ := ⟨x, hx, @@is_ideal.zero _ x.1 x.2.2.to_is_ideal⟩,
---     add_  := λ x y ⟨Sx, hxc, hx⟩ ⟨Sy, hyc, hy⟩,
---       or.cases_on (hc Sx Sy hxc hyc)
---         (λ hxy, ⟨Sy, hyc, @@is_ideal.add _ Sy.2.2.to_is_ideal (hxy hx) hy⟩)
---         (λ hyx, ⟨Sx, hxc, @@is_ideal.add _ Sx.2.2.to_is_ideal hx (hyx hy)⟩),
---     smul  := λ x y ⟨Sy, hyc, hy⟩,
---       ⟨Sy, hyc, @@is_ideal.mul_left _ Sy.2.2.to_is_ideal hy⟩,
---     ne_univ := λ h, by rw set.eq_univ_iff_forall at h;
---       rcases h 1 with ⟨S, hsc, hs⟩; apply S.2.2.ne_univ;
---       exact @@is_submodule.univ_of_one_mem _ S.1
---         S.2.2.to_is_ideal.to_is_submodule hs }⟩,
--- λ S hsc z hzs, ⟨S, hsc, hzs⟩⟩
-
-def find_maximal_ideal : set α :=
+def find_maximal_ideal : ideal α :=
 (classical.some find_maximal_ideal.aux).1
 
 theorem find_maximal_ideal.contains : P ⊆ find_maximal_ideal :=
 (classical.some find_maximal_ideal.aux).2.1
 
 def find_maximal_ideal.is_maximal_ideal :
-  is_maximal_ideal find_maximal_ideal :=
-let M : {S : set α // P ⊆ S ∧ is_proper_ideal S} :=
-classical.some find_maximal_ideal.aux in
-{ eq_or_univ_of_subset := λ T ht hmt, or_iff_not_imp_right.2 $
-    λ h, congr_arg subtype.val $
-    classical.some_spec find_maximal_ideal.aux
-    ⟨T, set.subset.trans M.2.1 hmt, { ne_univ := h, .. ht }⟩ hmt,
-  ..M.2.2 }
-
-end maximal_ideal
-
-section nonunits
-
-parameters {α : Type u} [comm_ring α]
-parameters (x : α) (hx : x ∈ nonunits α)
-
-include hx
-
-theorem ne_univ_of_nonunits : span ({x}:set α) ≠ set.univ :=
-begin
-  intro h,
-  rw [span_singleton, set.eq_univ_iff_forall] at h,
-  exact hx (h 1)
+  ideal.is_maximal find_maximal_ideal :=
+let HM := classical.some_spec find_maximal_ideal.aux in
+let M := classical.some find_maximal_ideal.aux in
+begin 
+  split,
+  { exact M.2.2, },
+  { intros J HJ,
+    by_contra HC,
+    have HPJ : P ⊆ J := set.subset.trans find_maximal_ideal.contains HJ.1,
+    have HMJ := (HM ⟨J, HPJ, HC⟩) HJ.1,
+    have HnMJ := ne_of_lt HJ,
+    apply HnMJ,
+    exact subtype.ext.1 HMJ.symm, }
 end
 
-def find_maximal_ideal.of_nonunits : set α :=
-@@find_maximal_ideal _ (span ({x}:set α))
-{ ne_univ := ne_univ_of_nonunits }
-
-theorem find_maximal_ideal.of_nonunits.mem :
-  x ∈ find_maximal_ideal.of_nonunits :=
-@@find_maximal_ideal.contains _ (span ({x}:set α))
-{ ne_univ := ne_univ_of_nonunits } $
-subset_span $ set.mem_singleton x
-
-def find_maximal_ideal.of_nonunits.is_maximal_ideal :
-  is_maximal_ideal find_maximal_ideal.of_nonunits :=
-@@find_maximal_ideal.is_maximal_ideal _ (span ({x}:set α))
-{ ne_univ := ne_univ_of_nonunits }
-
-end nonunits
-
-section zero_ne_one
-
-parameters {α : Type u} [comm_ring α]
-parameters (hzo : (0:α) ≠ 1)
-
-include hzo
-
-def find_maximal_ideal.of_zero_ne_one : set α :=
-find_maximal_ideal.of_nonunits 0 $
-λ ⟨y, h⟩, hzo $ by simpa using h
-
-def find_maximal_ideal.of_zero_ne_one.is_maximal_ideal :
-  is_maximal_ideal find_maximal_ideal.of_zero_ne_one :=
-find_maximal_ideal.of_nonunits.is_maximal_ideal 0 $
-λ ⟨y, h⟩, hzo $ by simpa using h
-
-end zero_ne_one
-
-end is_ideal
+end find_maximal_ideal
