2e2a73b1b4021f4767fa0f452cfacb0ee8c8a7bb
Ramon Fernandez I Mir <rfernandezmir@gmail.com>
2019-04-03 15:07:25 +0100

    Working towards nicer more general localization predicates for the structure presheaf.

--- src/spectrum_of_a_ring/structure_presheaf.lean
+++ src/spectrum_of_a_ring/structure_presheaf.lean
@@ -25,34 +25,22 @@ open localization_alt
 
 section maps
 
-variable {φ : Π (f : R), R → localization.away f}
-variable {Hφ : ∀ f, is_ring_hom (φ f)}
-variable {Hloc : ∀ f, is_localization_data (powers f) (φ f)}
-
 -- D(g) ⊆ D(f) → f ∈ R[1/g]*
 
-section inverts
-
-include Hloc
-
-lemma localisation_inverts {f g : R} (H : Spec.D'(g) ⊆ Spec.D'(f)) 
-: inverts (powers f) (φ g) :=
+lemma localization.inverts.of_Dfs_subset {f g : R} (H : Spec.D'(g) ⊆ Spec.D'(f)) 
+: inverts (powers f) (localization.of : R → localization R (powers g)) :=
 begin
-  rcases (Hloc g) with ⟨Hinv, Hden, Hker⟩,
   rintros ⟨fn, Hfn⟩,
-  suffices Hsuff : ∃ si, φ g f * si = 1,
+  suffices Hsuff : ∃ si, (localization.of : R → localization R (powers g)) f * si = 1,
     rcases Hsuff with ⟨si, Hsi⟩,
-    show ∃ si, φ g fn * si = 1,
+    show ∃ si, localization.of fn * si = 1,
     rcases Hfn with ⟨n, Hfn⟩,
     rw ←Hfn,
     clear Hfn,
     induction n with n Hn,
-    { simp,
-      rw (is_ring_hom.map_one (φ g)),
-      use 1,
-      ring, },
+    { simp, },
     { rw pow_succ,
-      rw (is_ring_hom.map_mul (φ g)),
+      rw (@is_ring_hom.map_mul _ _ _ _ localization.of localization.of.is_ring_hom),
       rcases Hn with ⟨sin, Hn⟩,
       existsi (si * sin),
       rw ←mul_assoc,
@@ -67,48 +55,48 @@ begin
   unfold Spec.V' at H,
   by_contra Hne,
   rw not_exists at Hne,
-  have Hnu : ¬is_unit (φ g f),
+  have Hnu : ¬is_unit ((localization.of : R → localization R (powers g)) f),
     intros HC,
     simp [is_unit] at HC,
     rcases HC with ⟨u, HC⟩,
     apply (Hne u.inv),
     rw HC,
     exact u.3,
-  let F : ideal (localization.away g) := ideal.span {(φ g f)},
+  let F : ideal (localization.away g) := ideal.span {(localization.of f)},
   have HFnT : F ≠ ⊤,
     intros HC,
     rw ideal.span_singleton_eq_top at HC,
     exact (Hnu HC),
   rcases (ideal.exists_le_maximal F HFnT) with ⟨S, ⟨HMS, HFS⟩⟩,
-  have HfF : φ g f ∈ F,
-    suffices Hsuff : φ g f ∈ {φ g f},
+  have HfF : localization.of f ∈ F,
+    suffices Hsuff : localization.of f ∈ {localization.of f},
       exact ideal.subset_span Hsuff,
     exact set.mem_singleton _,
-  have HfM : φ g f ∈ S := HFS HfF,
+  have HfM : localization.of f ∈ S := HFS HfF,
   have PS := ideal.is_maximal.is_prime HMS,
-  have PS' : ideal.is_prime (ideal.comap (φ g) S)
-    := @ideal.is_prime.comap _ _ _ _ (φ g) _ _ PS,
-  let S' : Spec R := ⟨ideal.comap (φ g) S, PS'⟩,
+  have PS' : ideal.is_prime (ideal.comap localization.of S)
+    := @ideal.is_prime.comap _ _ _ _ localization.of _ _ PS,
+  let S' : Spec R := ⟨ideal.comap localization.of S, PS'⟩,
   have HfS' : f ∈ S'.val,
     rw ideal.mem_comap,
     exact HfM,
   replace HfS' : S' ∈ {P : Spec R | f ∈ P.val} := HfS',
-  have HgS' : g ∈ ideal.comap (φ g) S := H HfS',
+  have HgS' : g ∈ ideal.comap localization.of S := H HfS',
   rw ideal.mem_comap at HgS',
-  rcases (Hinv ⟨g, ⟨1, by simp⟩⟩) with ⟨w, Hw⟩,
-  have HC : φ g g * w ∈ S := ideal.mul_mem_right S HgS',
-  erw Hw at HC,
+  rcases (localization.coe_is_unit R (powers g) ⟨g, ⟨1, pow_one g⟩⟩) with ⟨w, Hw⟩,
+  rcases w with ⟨w, winv, Hwwinv, Hwinvw⟩,
+  change localization.of g = w at Hw,
+  have HC : localization.of g * winv ∈ S := ideal.mul_mem_right S HgS',
+  erw [Hw, Hwwinv] at HC,
   exact ((ideal.ne_top_iff_one S).1 PS.1) HC,
 end
 
-end inverts
+-- D(g) ⊆ D(f) → ∃ a e, g^e = a * f
 
 lemma localisation_inverts₂ {f g : R} (H : Spec.D'(g) ⊆ Spec.D'(f)) 
 : ∃ (a : R) (e : ℕ), g^e = a * f :=
 begin 
-  have Hinv := @localisation_inverts R _ 
-      (λ f, localization.of) (λ f, localization.of.is_ring_hom) 
-      (λ f, localization.of.is_localization_data (powers f)) _ _ H,
+  have Hinv := localization.inverts.of_Dfs_subset H,
   rcases (Hinv ⟨f, ⟨1, pow_one f⟩⟩) with ⟨w, Hw⟩,
   dsimp only [subtype.coe_mk] at Hw,
   rcases (quotient.exists_rep w) with ⟨⟨a, ⟨gn, ⟨n, Hn⟩⟩⟩, Hagn⟩,
@@ -122,7 +110,9 @@ end
 
 end maps
 
--- Use this to get a map ψ : Rf → Rg by the universal property.
+section localization_S
+
+-- Define S(U) so that R[1/f] ≃ R[1/S(D(f))].
 
 def S (U : opens (Spec R)) : set R := { r : R | U.1 ⊆ Spec.D'(r) }
 
@@ -137,11 +127,38 @@ instance S.is_submonoid (U : opens (Spec R)) : is_submonoid (S U) :=
 lemma S.rev_mono {U V : opens (Spec R)} (HVU : V ⊆ U) : S U ⊆ S V :=
 λ x Hx, set.subset.trans HVU Hx
 
-def φ' (f : R) : R → localization R (S (Spec.DO R (f))) := localization.of
+lemma S.f_mem (f : R) : f ∈ S (Spec.DO R (f)) := set.subset.refl _
 
-instance φ'.is_ring_hom (f : R) : is_ring_hom (φ' f) := localization.of.is_ring_hom
+lemma localization.SDf.inverts_data (f : R) 
+: inverts_data (powers f) (localization.of : R → localization R (S (Spec.DO R (f)))) :=
+begin
+  rintros ⟨s, Hs⟩,
+  have HsS : s ∈ S (Spec.DO R (f)) := (is_submonoid.power_subset (S.f_mem f)) Hs,use [⟦⟨1, ⟨s, HsS⟩⟩⟧],
+  apply quotient.sound,
+  use [1, is_submonoid.one_mem _],
+  dsimp only [subtype.coe_mk, one_mul, mul_one],
+end
 
-lemma RS (f : R) : is_localization_data (powers f) (φ' f) :=
+lemma localization.SDf.has_denom_data (f : R) 
+: has_denom_data (powers f) (localization.of : R → localization R (S (Spec.DO R (f)))) :=
+begin
+  intros x,
+  have Hx := quotient.exists_rep x,
+  rcases (classical.indefinite_description _ Hx) with ⟨⟨p, q⟩, Hpq⟩,
+  rcases q with ⟨q, Hq⟩,
+  dsimp [S, Spec.DO] at Hq,
+  rcases (classical.indefinite_description _ (localisation_inverts₂ Hq)) with ⟨a, Ha⟩,
+  rcases (classical.indefinite_description _ Ha) with ⟨e, Hfe⟩,
+  use [⟨⟨f^e, ⟨e, rfl⟩⟩, a * p⟩],
+  dsimp only [subtype.coe_mk],
+  rw [Hfe, ←Hpq],
+  apply quotient.sound,
+  use [1, is_submonoid.one_mem _],
+  dsimp,
+  ring,
+end
+
+lemma localization.SDf (f : R) : is_localization_data (powers f) (localization.of : R → localization R (S (Spec.DO R (f))) :=
 begin
   have HfS : f ∈ S (Spec.DO R (f)) := set.subset.refl _,
   refine ⟨_, _, _⟩,
@@ -183,11 +200,16 @@ begin
     { refl, } }
 end
 
+end localization_S
+
+section structure_presheaf
+
 variable (R)
 
+-- Structure presheaf on Spec(R) defined on the basis.
+
 def structure_presheaf_on_basis : presheaf_of_rings_on_basis (Spec R) (D_fs_basis R) := 
-{ -- F(D(f)) = R[1/S] ≅ R[1/f]
-  F := λ U BU, localization R (S U),
+{ F := λ U BU, localization R (S U),
   res := λ U V BU BV HVU,
     begin
       have H := S.rev_mono HVU,
@@ -208,3 +230,8 @@ def structure_presheaf : presheaf_of_rings (Spec R) :=
   presheaf_of_rings_on_basis_to_presheaf_of_rings 
     (D_fs_standard_basis R) 
     (structure_presheaf_on_basis R)
+
+-- lemma structure_presheaf.is_localization_data : 
+-- ∀ (f : R), is_localization_data ((structure_presheaf R).F (Spec.DO R f))
+
+end structure_presheaf
