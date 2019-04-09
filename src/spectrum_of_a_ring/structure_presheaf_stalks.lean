85cfa8537ac60169efbc4244873a2a6b8cd6f2eb
Ramon Fernandez I Mir <rfernandezmir@gmail.com>
2019-04-09 16:21:04 +0100

    Stalks of the structure presheaf are local rings! Or rather have the localization property of R_P.

--- src/spectrum_of_a_ring/structure_presheaf_stalks.lean
+++ src/spectrum_of_a_ring/structure_presheaf_stalks.lean
@@ -8,6 +8,8 @@ import spectrum_of_a_ring.structure_presheaf_res
 
 universe u
 
+noncomputable theory
+
 variables {R : Type u} [comm_ring R]
 variables (P : Spec R)
 
@@ -20,6 +22,7 @@ def F := structure_presheaf_on_basis R
 
 def FP := stalk_of_rings_on_standard_basis Bstd F P
 
+-- This is essentially the map from x to x/1
 def φ : R → FP P := λ x, 
 ⟦{ U := opens.univ,
    BU := (D_fs_standard_basis R).1,
@@ -62,7 +65,7 @@ instance φP.is_ring_hom : is_ring_hom (φ P) :=
 
 -----
 
-lemma stalk_local.inverts : inverts_data (-P.1 : set R) (φ P) :=
+lemma stalk_local.inverts_data : inverts_data (-P.1 : set R) (φ P) :=
 begin
   rintros ⟨s, Hs⟩,
   change s ∉ P.val at Hs,
@@ -88,18 +91,53 @@ begin
   simp,
 end
 
-lemma stalk_local.has_denom : has_denom_data (-P.1 : set R) (φ P) :=
+lemma stalk_local.has_denom : has_denom (-P.1 : set R) (φ P) :=
 begin
-  sorry,
+  intros x,
+  apply quotient.induction_on x,
+  rintros ⟨U, BU, HPU, s⟩,
+  -- s ∈ R[1/S(U)]. 
+  apply quotient.induction_on s,
+  rintros ⟨p, ⟨q, Hq⟩⟩,
+  change U ⊆ Spec.DO R q at Hq,
+  -- Since P ∈ D(q).
+  have HqnP : q ∉ P.val := Hq HPU,
+  use [⟨⟨q, HqnP⟩, p⟩],
+  apply quotient.sound,
+  use [U, BU, HPU, (λ x Hx, ⟨trivial, Hx⟩), set.subset_univ _],
+  simp,
+  erw (F.res_is_ring_hom _ _ _).map_mul,
+  iterate 2 { rw ←presheaf_on_basis.Hcomp', },
+  erw presheaf_on_basis.Hid',
+  erw structure_presheaf_on_basis.res_eq,
+  iterate 2 { erw ←structure_presheaf_on_basis.res_comp_of', },
+  apply quotient.sound,
+  use [1, is_submonoid.one_mem _],
+  simp,
 end
+
+lemma stalk_local.has_denom_data : has_denom_data (-P.1 : set R) (φ P) :=
+has_denom_some (-P.1 : set R) (φ P) (stalk_local.has_denom P)
 
 lemma stalk_local.ker_le : ker (φ P) ≤ submonoid_ann (-P.1 : set R) :=
 begin
-  sorry,
+  intros x Hx,
+  change φ P x = 0 at Hx,
+  erw quotient.eq at Hx,
+  rcases Hx with ⟨W, BW, HPW, HWuniv1, HWuniv2, Hres⟩,
+  dsimp at Hres,
+  erw is_ring_hom.map_zero (F.res (D_fs_standard_basis R).1 BW HWuniv2) at Hres,
+  erw structure_presheaf_on_basis.res_eq at Hres,
+  erw ←structure_presheaf_on_basis.res_comp_of' at Hres,
+  erw quotient.eq at Hres,
+  rcases Hres with ⟨t, HtSW, Ht⟩,
+  simp at Ht,
+  have HtnP : t ∉ P.val := HtSW HPW,
+  use ⟨⟨x, ⟨t, HtnP⟩⟩, Ht⟩,
 end
 
 lemma stalk_local : is_localization_data (-P.1 : set R) (φ P) :=
-{ inverts := stalk_local.inverts P, 
-  has_denom := stalk_local.has_denom P, 
+{ inverts := stalk_local.inverts_data P, 
+  has_denom := stalk_local.has_denom_data P, 
   ker_le := stalk_local.ker_le P, }
 
