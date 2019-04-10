f8df2f257ae9823a12705e9a270c55032687dcd3
Ramon Fernandez I Mir <rfernandezmir@gmail.com>
2019-04-10 13:46:13 +0100

    Fixed imports in scheme.lean.

--- src/scheme.lean
+++ src/scheme.lean
@@ -1,5 +1,11 @@
+/-
+  Definition of a scheme.
+
+  https://stacks.math.columbia.edu/tag/01II
+-/
+
 import topology.basic
-import preliminaries.covering
+import sheaves.covering.covering
 import sheaves.presheaf_of_rings_maps
 import sheaves.locally_ringed_space
 import spectrum_of_a_ring.zariski_topology
@@ -11,10 +17,12 @@ universes u
 
 open presheaf_of_rings
 
+-- A scheme is a locally ringed space covered by affine schemes.
+
 structure scheme (α : Type u) [topological_space α] :=
 (carrier    : locally_ringed_space α)
 (cov        : covering.univ α)
 (Haffinecov : 
   ∀ i, ∃ (R) (H : comm_ring R) (fpU : pullback carrier.O.F),
       fpU.range = cov.Uis i
-    ∧ fpU.carrier ≅ structure_sheaf.presheaf R)
+    ∧ nonempty (fpU.carrier ≅ structure_sheaf.presheaf R))
