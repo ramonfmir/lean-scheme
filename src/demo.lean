0a6824cd56de49647e836d429e6b7c823a39bcc6
Ramon Fernandez I Mir <rfernandezmir@gmail.com>
2019-06-23 22:12:50 +0100

    Small changes in demo.

--- src/demo.lean
+++ src/demo.lean
@@ -1,16 +1,16 @@
--- The natural numbers
+-- The natural numbers.
 inductive mynat 
 | zero : mynat
 | succ : mynat → mynat
 
 variable (R : mynat → mynat → Prop)
 
--- Term mode
+-- Term mode.
 lemma forall_exists_of_exists_forall
 : (∃ x, ∀ y, R x y) → (∀ y, ∃ x, R x y) :=
 λ ⟨a, Ha⟩ b, ⟨a, Ha b⟩
 
--- Tactic mode
+-- Tactic mode.
 lemma forall_exists_of_exists_forall' 
 : (∃ x, ∀ y, R x y) → (∀ y, ∃ x, R x y) :=
 begin
@@ -20,10 +20,11 @@ begin
   exact (Ha b),
 end
 
--- Suppose R is a function
+-- Suppose R is a function.
 axiom is_function : ∀ x, ∃! y, R x y
 
--- Classical library example
+-- Classical library example.
 noncomputable def find_image
 : Π n, {m // R n m} :=
-λ n, classical.choice (let ⟨m, ⟨Hm, _⟩⟩ := is_function R n in ⟨⟨m, Hm⟩⟩)
+λ n, classical.choice 
+  (let ⟨m, ⟨Hm, _⟩⟩ := is_function R n in ⟨⟨m, Hm⟩⟩)
